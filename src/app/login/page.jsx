fd83498bdf0b4c7e6f6c4c23cdec83ee337221a6
Hammix3112 <hamaadjanjua64@gmail.com>
2025-07-03 13:11:09 +0500

    scale-dashboard

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Login from '../components/login'
+import React from "react";
+import Login from "../components/login";
 
 const page = () => {
   return (
     <div>
-      <Login/>
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
